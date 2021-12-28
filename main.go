b2cf1458042579ce3118c2afb84dc55e42e1f136
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2021-12-28 18:43:20 +0100

    ch01

--- main.go
+++ main.go
@@ -2,10 +2,41 @@ package main
 
 import "fmt"
 
-func Sum(x int, y int) int {
-	return x + y
+type Tuple struct {
+	x float64
+	y float64
+	z float64
+	w float64
+}
+
+func NewTuple(x, y, z, w float64) *Tuple {
+	return &Tuple{x, y, z, w}
+}
+
+func NewVector(x, y, z float64) *Tuple {
+	return NewTuple(x, y, z, 0)
+}
+
+func NewPoint(x, y, z float64) *Tuple {
+	return NewTuple(x, y, z, 1)
+}
+
+func (a *Tuple) String() string {
+	return fmt.Sprintf("Tuple(%f, %f, %f, %f)", a.x, a.y, a.z, a.w)
+}
+
+func (a *Tuple) Add(b *Tuple) *Tuple {
+	return NewTuple(a.x+b.x, a.y+b.y, a.z+b.z, a.w+b.w)
+}
+
+func (a *Tuple) Sub(b *Tuple) *Tuple {
+	return NewTuple(a.x-b.x, a.y-b.y, a.z-b.z, a.w-b.w)
+}
+
+func (a *Tuple) Negate() *Tuple {
+	return NewTuple(-a.x, -a.y, -a.z, -a.w)
 }
 
 func main() {
-	fmt.Println(Sum(5, 5))
+	fmt.Println("0")
 }
--- main_test.go
+++ main_test.go
@@ -1,10 +1,73 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
-func TestSum(t *testing.T) {
-	total := Sum(5, 5)
-	if total != 10 {
-		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+func TestAddingTwoTuple(t *testing.T) {
+	e := NewTuple(1, 1, 6, 1)
+	x := NewTuple(3, -2, 5, 1)
+	y := NewTuple(-2, 3, 1, 0)
+	a := x.Add(y)
+	assertTupleEqual(t, a, e)
+}
+
+func TestSubtractingTwoPoints(t *testing.T) {
+	e := NewVector(-2, -4, -6)
+	x := NewPoint(3, 2, 1)
+	y := NewPoint(5, 6, 7)
+	a := x.Sub(y)
+	assertTupleEqual(t, a, e)
+}
+
+func TestSubtractingAVectorFromAPoint(t *testing.T) {
+	e := NewPoint(-2, -4, -6)
+	x := NewPoint(3, 2, 1)
+	y := NewVector(5, 6, 7)
+	a := x.Sub(y)
+	assertTupleEqual(t, a, e)
+}
+
+func TestSubtractingTwoVectors(t *testing.T) {
+	e := NewVector(-2, -4, -6)
+	x := NewVector(3, 2, 1)
+	y := NewVector(5, 6, 7)
+	a := x.Sub(y)
+	assertTupleEqual(t, a, e)
+}
+
+func TestSubtractingAVectorFromTheZeroVector(t *testing.T) {
+	e := NewVector(-1, 2, -3)
+	x := NewVector(0, 0, 0)
+	y := NewVector(1, -2, 3)
+	a := x.Sub(y)
+	assertTupleEqual(t, a, e)
+}
+
+func TestNegatingATuple(t *testing.T) {
+	e := NewTuple(-1, 2, -3, 4)
+	x := NewTuple(1, -2, 3, -4)
+	a := x.Negate()
+	assertTupleEqual(t, a, e)
+}
+
+const float64EqualityThreshold = 1e-6
+
+func floatEqual(a, b float64) bool {
+	return math.Abs(a-b) <= float64EqualityThreshold
+}
+
+func tupleEqual(a, b *Tuple) bool {
+	return floatEqual(a.x, b.x) &&
+		floatEqual(a.y, b.y) &&
+		floatEqual(a.z, b.z) &&
+		floatEqual(a.w, b.w)
+}
+
+func assertTupleEqual(t *testing.T, a, e *Tuple) {
+	t.Helper()
+	if !tupleEqual(a, e) {
+		t.Errorf("Sum was incorrect, \n got: %v \n want: %v", a, e)
 	}
 }
